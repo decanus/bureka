4e66d8c64121dd80a2c0b0046cdfa2bb828e3c66
decanus <7621705+decanus@users.noreply.github.com>
2020-05-04 18:47:47 +0200

    started outline

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pastry
-a libp2p compatible implementation of the pastry DHT in go
+
+**:warning: WORK IN PROGRESS! :warning:**
+
+A libp2p compatible implementation of the pastry DHT in go
---
+++ pastry.go
@@ -0,0 +1,44 @@
+package pastry
+
+import (
+	"context"
+	logging "github.com/ipfs/go-log"
+	"github.com/libp2p/go-libp2p-core/peer"
+)
+
+var logger = logging.Logger("dht")
+
+type Pastry struct {
+
+}
+
+func (p *Pastry) Route() {
+
+}
+
+func (p *Pastry) Deliver() {
+
+}
+
+func (p *Pastry) Forward() {
+
+}
+
+func (p *Pastry) FindPeer(ctx context.Context, id peer.ID) (peer.AddrInfo, error) {
+	if err := id.Validate(); err != nil {
+		return peer.AddrInfo{}, err
+	}
+
+	logger.Debug("finding peer", "peer", id)
+
+	local := p.FindLocal(id)
+	if local != nil {
+		return *local, nil
+	}
+
+	return peer.AddrInfo{}, nil
+}
+
+func (p *Pastry) FindLocal(id peer.ID) *peer.AddrInfo {
+	return nil
+}
