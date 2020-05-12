c216a7c399349ec4b25c40512e455583eeefd2ab
decanus <7621705+decanus@users.noreply.github.com>
2020-05-12 13:47:46 +0200

    started working on routing tbale

--- state/routingtable.go
+++ state/routingtable.go
@@ -5,6 +5,17 @@ import "github.com/libp2p/go-libp2p-core/peer"
 type RoutingTable [][]peer.AddrInfo
 
 func (r RoutingTable) Route(self, target peer.ID) *peer.AddrInfo {
-	// @TODO
+	p := commonPrefix(self, target)
+
+	if p >= len(r) {
+		// @todo
+	}
+
+	//row := r[p]
+
 	return nil
+}
+
+func commonPrefix(self, target peer.ID) int {
+	return 0
 }
