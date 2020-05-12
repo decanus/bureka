d3edc4130e2d8719a1c47cf74feec232cb1948c1
decanus <7621705+decanus@users.noreply.github.com>
2020-05-12 17:21:39 +0200

    rearrange

--- state/leafset.go
+++ state/leafset.go
@@ -20,17 +20,6 @@ func NewLeafSet(key peer.ID) LeafSet {
 	}
 }
 
-// Closest returns the closest PeerInfo.
-func (l LeafSet) Closest(id peer.ID) *peer.AddrInfo {
-	byteid, _ := id.MarshalBinary()
-	k, _ := l.key.MarshalBinary()
-	if bytes.Compare(byteid, k) < 0 {
-		return l.smaller.Closest(id)
-	}
-
-	return l.larger.Closest(id)
-}
-
 // Insert inserts a peer in the LeafSet.
 func (l *LeafSet) Insert(peer *peer.AddrInfo) {
 	byteid, _ := peer.ID.MarshalBinary()
@@ -56,6 +45,17 @@ func (l *LeafSet) Remove(id peer.ID) bool {
 	larger, ok := l.larger.Remove(id)
 	l.larger = larger
 	return ok
+}
+
+// Closest returns the closest PeerInfo.
+func (l LeafSet) Closest(id peer.ID) *peer.AddrInfo {
+	byteid, _ := id.MarshalBinary()
+	k, _ := l.key.MarshalBinary()
+	if bytes.Compare(byteid, k) < 0 {
+		return l.smaller.Closest(id)
+	}
+
+	return l.larger.Closest(id)
 }
 
 // Min returns the farthest key to the smaller side.
