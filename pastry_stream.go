aca1bd2e592ec015eeb815add54a1171fb363d68
decanus <7621705+decanus@users.noreply.github.com>
2020-05-13 18:44:25 +0200

    protobuf

--- pastry_stream.go
+++ pastry_stream.go
@@ -1,6 +1,17 @@
 package pastry
 
-import "github.com/libp2p/go-libp2p-core/network"
+import (
+	"time"
+
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-msgio"
+
+	"github.com/decanus/pastry/pb"
+)
+
+var dhtReadMessageTimeout = 10 * time.Second
+var dhtStreamIdleTimeout = 1 * time.Minute
+
 
 func (p *Pastry) streamHandler(stream network.Stream) {
 	defer stream.Reset()
@@ -11,5 +22,23 @@ func (p *Pastry) streamHandler(stream network.Stream) {
 }
 
 func (p *Pastry) handleMessage(s network.Stream) bool {
+	r := msgio.NewVarintReaderSize(s, network.MessageSizeMax)
+
+	peer := s.Conn().RemotePeer()
+
+	timer := time.AfterFunc(dhtStreamIdleTimeout, func() { _ = s.Reset() })
+	defer timer.Stop()
+
+	for {
+		var req pb.Message
+		msgbytes, err := r.ReadMsg()
+		if err != nil {
+			continue
+		}
+
+		msgLen := len(msgbytes)
+
+	}
+
 	return true
 }
