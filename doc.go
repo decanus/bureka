64d041e80b30b252e0d570daa526141aff14305c
Dean Eigenmann <7621705+decanus@users.noreply.github.com>
2020-05-17 15:53:24 +0200

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package bureka implements a pastry-like DHT over libp2p.
+//
+// The implementation is inspired by https://github.com/libp2p/go-libp2p-kad-dht,
+// as well as various Node implementations including https://github.com/secondbit/wendy.
+package bureka
