1ed53197c5c322cfe4672b752afa17c2b16c516e
Dean Eigenmann <7621705+decanus@users.noreply.github.com>
2020-05-17 15:54:12 +0200

    Update doc.go

--- dht/doc.go
+++ dht/doc.go
@@ -1,5 +1,2 @@
-// Package dht implements a pastry node.
-//
-// The implementation is inspired by https://github.com/libp2p/go-libp2p-kad-dht,
-// as well as various Node implementations including https://github.com/secondbit/wendy.
+// Package dht contains an implementation of a pastry dht node.
 package dht
