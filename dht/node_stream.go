726d2acd509d537086c475aed07d186e9d4a0ae9
decanus <7621705+decanus@users.noreply.github.com>
2020-05-17 21:35:45 +0200

    lol dnt need

--- dht/node_stream.go
+++ dht/node_stream.go
@@ -4,7 +4,7 @@ import (
 	"io"
 	"time"
 
-	proto "github.com/gogo/protobuf/proto"
+	"github.com/gogo/protobuf/proto"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-msgio"
 	"github.com/pkg/errors"
