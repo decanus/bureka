2fdff593acbc1f3ed6281a5dbb7a3d5f0e64b839
decanus <7621705+decanus@users.noreply.github.com>
2020-05-19 17:25:06 +0200

    Applications

--- handlers.go
+++
@@ -1,8 +0,0 @@
-package bureka
-
-import "context"
-
-// @todo think about readers the same way github.com/uhthomas/pastry does
-
-type DeliverHandler func(ctx context.Context, key []byte) error
-type ForwardHandler func(ctx context.Context, next []byte, key []byte) error
