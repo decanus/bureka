251f83c17d37d7f13c4feae98a715d7df8ec35d8
Dean Eigenmann <7621705+decanus@users.noreply.github.com>
2020-05-21 15:25:39 +0200

    feature/send (#6)
    * started working on send
    
    * actually writing
    
    * updated
    
    * Update node.go
    
    * started working on a mock test
    
    * using mock
    
    * mock
    
    * gitattributes
    
    * Update Makefile
    
    * eol
    
    * bye
    
    * added method to connect 2 test dht
    
    * todo

--- dht/node.go
+++ dht/node.go
@@ -1,18 +1,22 @@
 package dht
 
 import (
+	"bufio"
 	"context"
 	"sync"
 
 	logging "github.com/ipfs/go-log"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/protocol"
 	"github.com/libp2p/go-libp2p-core/routing"
 
 	"github.com/decanus/bureka/state"
 )
 
 var logger = logging.Logger("dht")
+
+const pastry protocol.ID = "/pastry/1.0/proto"
 
 // Application represents a pastry application
 type Application interface {
@@ -29,7 +33,7 @@ type Node struct {
 	NeighborhoodSet state.Set
 	RoutingTable    state.RoutingTable
 
-	host host.Host
+	Host host.Host
 
 	applications []Application
 }
@@ -37,12 +41,13 @@ type Node struct {
 // Guarantee that we implement interfaces.
 var _ routing.PeerRouting = (*Node)(nil)
 
-func New(ctx context.Context, host host.Host) *Node {
+func New(ctx context.Context, host host.Host) (*Node, *Node) {
 	return &Node{
 		LeafSet:         state.NewLeafSet(host.ID()),
 		NeighborhoodSet: make(state.Set, 0),
 		applications:    make([]Application, 0),
-	}
+		Host:            host,
+	}, nil
 }
 
 // AddApplication adds an application as a message receiver.
@@ -51,6 +56,37 @@ func (n *Node) AddApplication(app Application) {
 	defer n.Unlock()
 
 	n.applications = append(n.applications, app)
+}
+
+// Send sends a message to the target or the next closest peer.
+func (n *Node) Send(ctx context.Context, msg []byte, key peer.ID) error {
+	if key == n.Host.ID() {
+		n.deliver(msg) // @todo we may need to do this for more than just message types, like when the routing table is updated.
+		return nil
+	}
+
+	target := n.route(key)
+	if target.ID == "" {
+		// no target to be found, delivering to self
+		return nil
+	}
+
+	forward := n.forward(msg, target.ID)
+	if !forward {
+		return nil
+	}
+
+	err := n.send(ctx, msg, target.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ID returns a nodes ID, mainly for testing purposes.
+func (n *Node) ID() peer.ID  {
+	return n.Host.ID()
 }
 
 func (n *Node) FindPeer(ctx context.Context, id peer.ID) (peer.AddrInfo, error) {
@@ -78,10 +114,55 @@ func (n *Node) route(to peer.ID) peer.AddrInfo {
 	}
 
 	// @todo this is flimsy but will fix later
-	addr := n.RoutingTable.Route(n.host.ID(), to)
+	addr := n.RoutingTable.Route(n.Host.ID(), to)
 	if addr != nil {
 		return *addr
 	}
 
 	return peer.AddrInfo{}
+}
+
+// deliver sends the message to all connected applications.
+func (n *Node) deliver(msg []byte) {
+	n.RLock()
+	defer n.RUnlock()
+
+	for _, app := range n.applications {
+		app.Deliver(msg)
+	}
+}
+
+// forward asks all applications whether a message should be forwarded to a peer or not.
+func (n *Node) forward(msg []byte, target peer.ID) bool {
+	n.RLock()
+	defer n.RUnlock()
+
+	// @todo need to run over this logic
+	forward := true
+	for _, app := range n.applications {
+		f := app.Forward(msg, target)
+		if forward {
+			forward = f
+		}
+	}
+
+	return forward
+}
+
+func (n *Node) send(ctx context.Context, msg []byte, target peer.ID) error {
+	s, err := n.Host.NewStream(ctx, target, pastry)
+	if err != nil {
+		return err
+	}
+
+	bufw := bufio.NewWriter(s)
+
+	// @todo probably needs this: https://github.com/libp2p/go-libp2p-pubsub/blob/5bbe37191afbb25a953e7931bf1a2ce18fbbb8f3/comm.go#L116
+
+	_, err = bufw.Write(msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
---
+++ dht/node_test.go
@@ -0,0 +1,66 @@
+package dht_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peerstore"
+	swarmt "github.com/libp2p/go-libp2p-swarm/testing"
+	bhost "github.com/libp2p/go-libp2p/p2p/host/basic"
+
+	"github.com/decanus/bureka/dht"
+	internal "github.com/decanus/bureka/dht/internal/mocks"
+)
+
+// @TODO MORE TESTS
+
+func setupDHT(ctx context.Context, t *testing.T) *dht.Node {
+	d, err := dht.New(
+		ctx,
+		bhost.New(swarmt.GenSwarm(t, ctx, swarmt.OptDisableReuseport)),
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return d
+}
+
+func connectNoSync(t *testing.T, ctx context.Context, a, b *dht.Node) {
+	t.Helper()
+
+	idB := b.ID()
+	addrB := b.Host.Peerstore().Addrs(idB)
+	if len(addrB) == 0 {
+		t.Fatal("peers setup incorrectly: no local address")
+	}
+
+	a.Host.Peerstore().AddAddrs(idB, addrB, peerstore.TempAddrTTL)
+	pi := peer.AddrInfo{ID: idB}
+	if err := a.Host.Connect(ctx, pi); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestNode_Send_To_Self(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	n := setupDHT(context.Background(), t)
+
+	mock := internal.NewMockApplication(ctrl)
+	n.AddApplication(mock)
+
+	msg := []byte("hello, world!")
+
+	mock.EXPECT().Deliver(gomock.Eq(msg)).Times(1)
+
+	err := n.Send(context.Background(), msg, n.ID())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
