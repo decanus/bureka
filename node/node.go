c3df13bf3b04384d56fcd023894fa57ea5a480ef
decanus <7621705+decanus@users.noreply.github.com>
2020-05-26 13:59:37 +0200

    remove io stuff for now

--- node/node.go
+++ node/node.go
@@ -1,9 +1,25 @@
 package node
 
-type Node struct {
+import (
+	"context"
 
+	"github.com/libp2p/go-libp2p-core/host"
+
+	"github.com/decanus/bureka/dht"
+	"github.com/decanus/bureka/dht/state"
+	"github.com/decanus/bureka/pb"
+)
+
+type Node struct {
+	dht  dht.DHT
+	host host.Host
 }
 
 func New() (*Node, error) {
 	return nil, nil
 }
+
+func (n *Node) Send(ctx context.Context, target state.Peer, msg pb.Message) error {
+	panic("implement me")
+}
+
